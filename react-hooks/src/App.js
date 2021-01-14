9feaf6046fca161f126784b880647f78b2bcf3c9
Mario Lazzari <mario.lazzari@gmail.com>
2021-01-14 20:17:55 +0100

    Done section 1

---
+++ react-hooks/src/App.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect, useCallback } from "react";
+
+const App = () => {
+  // state
+  const [news, setNews] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("react");
+  const [url, setUrl] = useState(
+    "http://hn.algolia.com/api/v1/search?query=react"
+  );
+  const [loading, setLoading] = useState(false);
+
+  // fetch news
+  const fetchNews = useCallback(() => {
+    // set loading true
+    setLoading(true);
+    fetch(url)
+      .then(result => result.json())
+      .then(data => {
+        setNews(data.hits);
+        setLoading(false);
+      })
+      .catch(error => console.log(error));
+  }, [url]);
+
+  useEffect(() => {
+    fetchNews();
+  }, [fetchNews]);
+
+  const handleChange = e => setSearchQuery(e.target.value);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`);
+  };
+
+  const showLoading = () => (loading ? <h2>Loading...</h2> : "");
+
+  const searchForm = () => (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={searchQuery} onChange={handleChange} />
+      <button>Search</button>
+    </form>
+  );
+
+  const showNews = () => news.map((n, i) => <p key={i}>{n.title}</p>);
+
+  return (
+    <div>
+      <h2>News</h2>
+      {showLoading()}
+      {searchForm()}
+      {showNews()}
+    </div>
+  );
+};
+
+export default App;
